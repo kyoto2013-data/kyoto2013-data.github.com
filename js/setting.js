32f1437fb3090ffb590ac3db20532b012e9742df
kyolib2013 <kyolib2008@gmail.com>
2013-04-26 18:51:36 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,7 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'kyotocity2013budget_240213_5_0_p152_4';
+OpenSpending.identifier = 'kyotocity2013budget';
 OpenSpending.year = '2013';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
