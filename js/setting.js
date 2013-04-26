c66179099ce30537ca7939e519d5ce500fc45eb4
kyolib2013 <kyolib2008@gmail.com>
2013-04-26 18:15:10 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -19,24 +19,31 @@ OpenSpending.Styles.Cofog = {
 
   /* kyoto city cofog */
   '2013_1_1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+
   '2013_2_1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
   '2013_2_2': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#0AB971' },
+
   '2013_3_1': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#0AB971' },
   '2013_3_2': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
+
   '2013_4_1': { icon: 'icons/economy-tourism.svg', color: '#C75746', bcolor: '#4E6D00' },
   '2013_4_2': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#4E6D00' },
   '2013_4_3': { icon: 'icons/harbor.svg', color: '#C75746', bcolor: '#4E6D00' },
   '2013_4_4': { icon: 'icons/culture.svg', color: '#C75746', bcolor: '#4E6D00' },
+
   '2013_5_1': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#D33673' },
   '2013_5_2': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#D33673' },
   '2013_5_3': { icon: 'icons/street-lights.svg', color: '#C75746', bcolor: '#D33673' },
+
   '2013_6_1': { icon: 'icons/environment.svg', color: '#C75746', bcolor: '#2A3A03' },
   '2013_6_2': { icon: 'icons/tree.svg', color: '#C75746', bcolor: '#2A3A03'  },
+
   '2013_7_1': { icon: 'icons/wind.svg', color: '#C75746', bcolor: '#2A3A03'  },
   '2013_7_2': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
   '2013_7_3': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
   '2013_7_4': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
   '2013_7_5': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
+
   '2013_8_1': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
 };
 
