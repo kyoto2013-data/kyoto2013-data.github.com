7e16ce81932311fb72a7352ebd72918185f9b5c7
kyolib2013 <kyolib2008@gmail.com>
2013-04-27 20:20:42 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -10,39 +10,41 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'kyotocity2013budget';
-OpenSpending.year = '2013';
+OpenSpending.identifier = 'yokohama_budget';
+OpenSpending.year = '2012';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
 
 OpenSpending.Styles.Cofog = {
 
-  /* kyoto city cofog */
-  '2013-0101': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-
-  '2013-0201': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-  '2013-0202': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#0AB971' },
-
-  '2013-0301': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2013-0302': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
-
-  '2013-0401': { icon: 'icons/economy-tourism.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '2013-0402': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '2013-0403': { icon: 'icons/harbor.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '2013-0404': { icon: 'icons/culture.svg', color: '#C75746', bcolor: '#4E6D00' },
-
-  '2013-0501': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#D33673' },
-  '2013-0502': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#D33673' },
-  '2013-0503': { icon: 'icons/street-lights.svg', color: '#C75746', bcolor: '#D33673' },
-
-  '2013-0601': { icon: 'icons/environment.svg', color: '#C75746', bcolor: '#2A3A03' },
-  '2013-0602': { icon: 'icons/tree.svg', color: '#C75746', bcolor: '#2A3A03'  },
-
-  '2013-0701': { icon: 'icons/wind.svg', color: '#C75746', bcolor: '#2A3A03'  },
-  '2013-0702': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
-  '2013-0703': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
-  '2013-0704': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
-  '2013-0705': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
-
-  '2013-0801': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' }
+  /* yokohama cofog */
+  '1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+  '1-1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+  '2': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#0AB971' },
+  '2-1': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#0AB971' },
+  '2-2': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
+  '3': { icon: 'icons/economy-tourism.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '3-1': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '3-2': { icon: 'icons/harbor.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '3-3': { icon: 'icons/culture.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '4': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#D33673' },
+  '4-1': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#D33673' },
+  '4-2': { icon: 'icons/street-lights.svg', color: '#C75746', bcolor: '#D33673' },
+  '4-3': { icon: 'icons/housing.svg', color: '#C75746', bcolor: '#D33673' },
+  '5': { icon: 'icons/environment.svg', color: '#C75746', bcolor: '#2A3A03' },
+  '5-1': { icon: 'icons/tree.svg', color: '#C75746', bcolor: '#2A3A03'  },
+  '5-2': { icon: 'icons/wind.svg', color: '#C75746', bcolor: '#2A3A03'  },
+  '6': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
+  '6-1': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
+  '7': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
+  '7-1': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
+  '8': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
+  '8-1': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#C75746' },
+  '8-2': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
+  '9': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#D33673' },
+  '9-1': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#D33673' },
+  '10': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' },
+  '10-1': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
 };
+
+
