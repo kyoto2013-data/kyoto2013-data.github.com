50b6a15ca44a34b2c5a327caa4609ba6ce22b08a
kyolib2013 <kyolib2008@gmail.com>
2013-05-01 00:45:33 +0900

    Update dailybread.js

--- js/dailybread.js
+++ js/dailybread.js
@@ -194,7 +194,7 @@ OpenSpending.DailyBread = function (elem) {
       var iconUrl, paper;
       iconUrl = $(e).data('svg-url');
       paper = Raphael(e, iconRad+iconRad,iconRad+iconRad+5);
-      paper.circle(iconRad,iconRad,iconRad).attr({ fill: '#00588C', stroke: 'none' });
+      paper.circle(iconRad,iconRad,iconRad).attr({ fill: '#8B3B24', stroke: 'none' });
       paper.circle(iconRad,iconRad,iconRad-2).attr({ fill: 'none', stroke: '#eee', opacity: .8, 'stroke-dasharray': '- ' });
       $.get(iconUrl, function(svg) {
         if (typeof(svg) == "string") {
